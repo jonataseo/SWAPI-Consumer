78546acd2e2a2b55f5a44d0e96897ffc0adb404b
Jonatas Ezequiel de Oliveira <jonatas.eoliveira@gmail.com>
2019-05-16 17:05:52 -0400

    Fixing bugs in view (Now are displaying the data correctlly)

--- app.js
+++ app.js
@@ -4,17 +4,33 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res, next) {
-    res.render('index', {title: 'A Simple SWAPI Consumer Using Node.js, Express.js and Pug.js', message: 'It works!!(Apache fellings)'})
+    res.render('index', {title: 'A Simple SWAPI Consumer Using Node.js, Express.js and Pug.js', 
+        message: 'It works!!(Apache fellings)', 
+        values: []})
 })
+
+
 
 app.get('/films', function(req, res, next)  {
     api_helper.make_API_call('https://swapi.co/api/films/')
     .then(response => {
-        res.json(response)
+        res.render('films', {response})
+    })
+    .catch(error => {
+        res.send(error)
+    })    
+})
+
+app.get('/films/:id', function(req, res, next) {
+    var url = `https://swapi.co/api/films/${req.params.id}`;
+    api_helper.make_API_call(url)
+    .then(response => {
+        res.render('filmsDetail', {response})
     })
     .catch(error => {
         res.send(error)
